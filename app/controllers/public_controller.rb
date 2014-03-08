1e33f969b14bee4733d345ea2e806ecaffcf9137
Arjun Venkataswamy <avenkat2@gmail.com>
2014-03-08 01:04:53 -0600

    makes background image dynamic with an API

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,17 @@
+require 'open-uri'
+require 'json'
+
 class PublicController < ApplicationController
   def random
     num_items = Item.count
     offset_items = Item.offset(rand(num_items))
     @item = offset_items.first
+
+    url = "http://www.loc.gov/pictures/search/?q=#{@item.category}&fo=json"
+    response = open(url).read
+    parsed_response = JSON.parse(response)
+    results = parsed_response["results"]
+
+    @image_url = results[0]['image']['full']
   end
 end
