78b67a4ae2777980aabfef60bf767c4bfeb78d8b
Arjun Venkataswamy <avenkat2@gmail.com>
2014-03-08 01:01:41 -0600

    randomizes items

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,7 @@
 class PublicController < ApplicationController
   def random
-    @item = Item.first
+    num_items = Item.count
+    offset_items = Item.offset(rand(num_items))
+    @item = offset_items.first
   end
 end
